8e7e63370857eb5aeaff8ed512436b02b74bd244
peter <tianxi.pan@netvuetech.com>
2018-04-15 23:28:03 +0800

    no message

--- quartz-demo/src/main/java/com/tx/demo/HelloJobListener.java
+++ quartz-demo/src/main/java/com/tx/demo/HelloJobListener.java
@@ -1,4 +1,35 @@
 package com.tx.demo;
 
-public class HelloJobListener {
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobListener;
+
+public class HelloJobListener implements JobListener{
+    public static final String LISTENER_NAME = "HelloJobListenerName";
+
+    public String getName() {
+        return LISTENER_NAME;
+    }
+
+    public void jobToBeExecuted(JobExecutionContext jobExecutionContext) {
+        String jobName = jobExecutionContext.getJobDetail().getKey().toString();
+        System.out.println("jobToBeExecuted");
+        System.out.println("Job : " + jobName + " is going to start...");
+    }
+
+    public void jobExecutionVetoed(JobExecutionContext jobExecutionContext) {
+        System.out.println("jobExecutionVetoed");
+    }
+
+    public void jobWasExecuted(JobExecutionContext jobExecutionContext, JobExecutionException e) {
+        System.out.println("jobWasExecuted");
+
+        String jobName = jobExecutionContext.getJobDetail().getKey().toString();
+        System.out.println("Job : " + jobName + " is finished...");
+
+        if (!e.getMessage().equals("")) {
+            System.out.println("Exception thrown by: " + jobName
+                    + " Exception: " + e.getMessage());
+        }
+    }
 }
--- quartz-demo/src/test/java/com/tx/demo/HelloJobTest.java
+++ quartz-demo/src/test/java/com/tx/demo/HelloJobTest.java
@@ -1,12 +1,11 @@
 package com.tx.demo;
 
 import org.junit.Test;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerFactory;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.impl.matchers.KeyMatcher;
 
+import static org.quartz.CronScheduleBuilder.cronSchedule;
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
@@ -16,6 +15,9 @@ import static org.quartz.TriggerBuilder.newTrigger;
  */
 public class HelloJobTest {
 
+    /*
+     * 简单作业调度
+     */
     @Test
     public void execute() throws Exception {
         SchedulerFactory schedFact = new StdSchedulerFactory();
@@ -39,6 +41,43 @@ public class HelloJobTest {
                 .build();
 
         //Tell quartz to schedule the job using our trigger
+        sched.scheduleJob(job, trigger);
+
+        Thread.currentThread().join(10 * 1000);
+    }
+
+    /*
+     * 带监听器的作业调度
+     */
+    @Test
+    public void execute2() throws Exception {
+        SchedulerFactory schedFact = new StdSchedulerFactory();
+        Scheduler sched = schedFact.getScheduler();
+
+
+        JobKey jobKey = new JobKey("myJob", "group1");
+
+        // define the job and tie it to HelloJob class
+        JobDetail job = newJob(HelloJob.class)
+                .withIdentity(jobKey)
+                .usingJobData("jobSays", "Hello World!")
+                .usingJobData("myFloatValue", 3.141f)
+                .build();
+
+
+        // Trigger the job to run now, and then every 40 seconds
+        Trigger trigger = newTrigger()
+                .withIdentity("myTrigger", "group1")
+                .startNow()
+                .withSchedule(
+                        cronSchedule("0/5 * * * * ?"))
+                .build();
+
+        //Listener attached to jobKey
+        sched.getListenerManager().addJobListener(new HelloJobListener(), KeyMatcher.keyEquals(jobKey));
+
+        //Tell quartz to schedule the job using our trigger
+        sched.start();
         sched.scheduleJob(job, trigger);
 
         Thread.currentThread().join(10 * 1000);
